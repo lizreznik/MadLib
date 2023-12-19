287f4af289216fd4ed5d8b90f03d5e962e28cf3c
lizreznik <525ereznik@frhsd.com>
2023-12-19 09:24:54 -0500

    updated text

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ let noun = prompt('Enter a noun:')
 let verb = prompt('Enter a verb:')
 let adverb = prompt('Enter an adverb:')
 
-let story = `Once upton a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}. `
+
+
+
+
+
+let story = `Once upton a time, in a <span class ="red">${adjective}</span>${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
 madLibOutputDiv.innerHTML = `<p>${story}<p>`
+
